5a7ee7bb8648494760baa41f1a80f458f6f92aff
danyprastya <danyprastyaalhakim@gmail.com>
2025-04-28 09:12:20 +0700

    first commit

---
+++ app/s3.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
+  region: process.env.AWS_REGION, // contoh: us-east-1
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const BUCKET_NAME = process.env.AWS_S3_BUCKET;
+
+async function listProductImages() {
+  const params = {
+    Bucket: BUCKET_NAME,
+    Prefix: 'products/' // Folder produk di dalam bucket
+  };
+
+  try {
+    const data = await s3.listObjectsV2(params).promise();
+    return data.Contents.map(item => {
+      return s3.getSignedUrl('getObject', {
+        Bucket: BUCKET_NAME,
+        Key: item.Key,
+        Expires: 3600 // 1 jam
+      });
+    });
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
+
+module.exports = { listProductImages };
---
+++ app/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const db = require('./db');
+const { listProductImages } = require('./s3');
+
+const app = express();
+const PORT = 3000;
+
+// Setup EJS view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', async (req, res) => {
+    try {
+      const images = await listProductImages(); // list dari S3
+      const [products] = await db.query('SELECT * FROM products'); // dari database
+
+      // Untuk tiap product, cari gambar yang cocok dari list images
+      const productsWithImages = products.map(product => {
+        const matchedImage = images.find(img => img.Key === product.image_key);
+        return {
+          ...product,
+          imageUrl: matchedImage ? `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${matchedImage.Key}` : null
+        };
+      });
+
+      res.render('index', { products: productsWithImages });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Something went wrong');
+    }
+});
+  
